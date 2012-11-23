6ff2221fa237f6024136cc945572fa2edc64f231
jdrydn <james@jdrydn.com>
2012-11-23 11:54:08 +0000

    Updating JS

--- js/tweets.js
+++ js/tweets.js
@@ -1,13 +1,7 @@
 	
 	$(document).ready(function() {
 		
-		$.getJSON('https://api.twitter.com/1/statuses/user_timeline/kenthack.json?callback=?', {count: 1}, function(data) {
-			console.log('1.1');
-			console.log(data);
-		});
-		
-		$.getJSON('https://api.twitter.com/1.1/statuses/user_timeline.json?callback=?', {count: 1, screen_name: 'kenthack'}, function(data) {
-			console.log('1.1');
+		$.getJSON('http://static.kenthack.com/tweet.latest.php', {count: 1}, function(data) {
 			console.log(data);
 		});
 		
