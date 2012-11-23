2ff542ae4175a13fb248ddd8a825c738fc9113b1
jdrydn <james@jdrydn.com>
2012-11-23 11:44:51 +0000

    Working on JS

--- js/tweets.js
+++ js/tweets.js
@@ -2,6 +2,12 @@
 	$(document).ready(function() {
 		
 		$.getJSON('https://api.twitter.com/1/statuses/user_timeline/kenthack.json', {count: 1}, function(data) {
+			console.log('1.1');
+			console.log(data);
+		});
+		
+		$.getJSON('https://api.twitter.com/1.1/statuses/user_timeline.json', {count: 1, screen_name: 'kenthack'}, function(data) {
+			console.log('1.1');
 			console.log(data);
 		});
 		
