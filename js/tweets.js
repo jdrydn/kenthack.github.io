75968466f87594395e2e07e74ed5c193c3a9a687
jdrydn <james@jdrydn.com>
2012-11-23 11:45:59 +0000

    Working on JS

--- js/tweets.js
+++ js/tweets.js
@@ -1,12 +1,12 @@
 	
 	$(document).ready(function() {
 		
-		$.getJSON('https://api.twitter.com/1/statuses/user_timeline/kenthack.json', {count: 1}, function(data) {
+		$.getJSON('https://api.twitter.com/1/statuses/user_timeline/kenthack.json?callback=?', {count: 1}, function(data) {
 			console.log('1.1');
 			console.log(data);
 		});
 		
-		$.getJSON('https://api.twitter.com/1.1/statuses/user_timeline.json', {count: 1, screen_name: 'kenthack'}, function(data) {
+		$.getJSON('https://api.twitter.com/1.1/statuses/user_timeline.json?callback=?', {count: 1, screen_name: 'kenthack'}, function(data) {
 			console.log('1.1');
 			console.log(data);
 		});
